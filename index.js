a4366f7d51f9ab6ab689743d2e791a5b34f59180
Sumit <Sumit03987@gmail.com>
2025-07-02 17:24:19 +0530

    index2

--- index.js
+++ index.js
@@ -17,11 +17,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// const corsOptions = {
+//     origin: 'https://job-frontend-dusky-phi.vercel.app',
+//     // origin:"https://job-hunt-frontend-t6um.onrender.com",
+//     // origin: ["http://localhost:5173", "https://your-frontend-service.onrender.com"],
+//     credentials: true,
+// };
+// app.use(cors(corsOptions));
+
 const corsOptions = {
-    origin: 'https://job-frontend-dusky-phi.vercel.app',
-    // origin:"https://job-hunt-frontend-t6um.onrender.com",
-    // origin: ["http://localhost:5173", "https://your-frontend-service.onrender.com"],
-    credentials: true,
+  origin: "https://job-frontend-dusky-phi.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
  
